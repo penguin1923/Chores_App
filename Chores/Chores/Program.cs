8fd57ad3747a838422fbabf1d68a7f7708ab36ac
penguin1923 <64542224+penguin1923@users.noreply.github.com>
2020-07-01 15:40:59 -0400

    i think this is the base sln

---
+++ Chores/Chores/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Chores
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public string CanItStart()
+        {
+            return "yes";
+
+        }
+    }
+}
---
+++ Chores/ChoresTests/ProgramTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chores.Tests
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void CanItStart()
+        {
+            Assert.AreEqual("yes", "yes");
+        }
+
+
+    }
+}
